651a06d66dc4e598cc955d9bac9641f106168e6c
Jesse Storimer <jstorimer@gmail.com>
2011-05-20 15:17:13 -0400

    Add logging

--- lib/opensrs/server.rb
+++ lib/opensrs/server.rb
@@ -7,13 +7,14 @@ module OpenSRS
   class BadResponse < StandardError; end
   
   class Server
-    attr_accessor :server, :username, :password, :key
+    attr_accessor :server, :username, :password, :key, :logger
 
     def initialize(options = {})
       @server   = URI.parse(options[:server] || "https://rr-n1-tor.opensrs.net:55443/")
       @username = options[:username]
       @password = options[:password]
       @key      = options[:key]
+      @logger   = options[:logger]
     end
 
     def call(options = {})
@@ -22,8 +23,10 @@ module OpenSRS
       }
       
       xml = xml_processor.build(attributes.merge!(options))
+      log(xml, "Request XML for #{options[:object]} #{options[:action]}")
 
       response        = http.post(server.path, xml, headers(xml))
+      log(response.body, "Response XML for #{options[:object]} #{options[:action]}")
       parsed_response = xml_processor.parse(response.body)
       
       return OpenSRS::Response.new(parsed_response, xml, response.body)
@@ -66,6 +69,14 @@ module OpenSRS
       http.use_ssl = (server.scheme == "https")
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       http
+    end
+
+    def log(data, message)
+      return unless logger
+
+      message = "[OpenSRS] " + message
+      line = [message, data].join("\n")
+      logger.info(line)
     end
   end
 end
